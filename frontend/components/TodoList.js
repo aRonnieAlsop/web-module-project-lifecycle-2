305db3e4bd52ba39779ca9f22bc2431ca20ba410
Arron Alsop <alsop.arron@gmail.com>
2023-10-03 22:32:54 -0700

    'wip'

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -8,8 +8,13 @@ export default class TodoList extends React.Component {
           <div>Walk the dog</div>
          {
           this.props.todos.reduce((acc, td) => {
-              if (this.props.displayCompleteds || !td.completed) return acc.concat(
-                <div onClick={this.props.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? ' ✔️' : '' }</div>
+              if (this.props.displayCompleteds || !td.completed) return 
+              acc.concat(
+                <TodoList
+                key={td.id}
+                  toggleCompleted={this.props.toggleCompleted}
+                  todo={td}
+                />
               )
               return acc
           }, [])
